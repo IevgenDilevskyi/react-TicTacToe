73ad2bee5cb664950475b07673b8a2b64fdc626a
Ievgen Dilevskyi <e.dilevskiy@gmail.com>
2024-02-02 10:40:02 -0700

    Avoiding intersecting States & Avoid Unnecessary State Management

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,21 +1,20 @@
-import { useState } from "react"
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null]
 ]
-export function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard)
+export function GameBoard({ onSelectSquare }) {
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard)
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard(prevGameBoard => {
-      const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol
-      return updatedBoard
-    })
-    onSelectSquare()
-  }
+  // function handleSelectSquare(rowIndex, colIndex) {
+  //   setGameBoard(prevGameBoard => {
+  //     const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol
+  //     return updatedBoard
+  //   })
+  //   onSelectSquare()
+  // }
+
   return (
     <ol id='game-board'>
       {gameBoard.map((row, rowIndex) => (
@@ -23,7 +22,7 @@ export function GameBoard({ onSelectSquare, activePlayerSymbol }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => { handleSelectSquare(rowIndex, colIndex) }}>{playerSymbol}</button>
+                <button onClick={onSelectSquare}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
