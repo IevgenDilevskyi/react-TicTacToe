322778e2a6f3886ca8173331cca1e4500eda0dd8
Ievgen Dilevskyi <e.dilevskiy@gmail.com>
2024-02-05 11:39:08 -0700

    Lifting Computed Values Up

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import Player from "./components/Player"
 import { GameBoard } from "./components/GameBoard"
 import { useState } from "react"
 import Log from "./components/Log"
+import { WINNING_COMBINATIONS } from "../winning-combinations"
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+]
 
 function getActivePlayer(gameTurns) {
   let currentPlayer = 'X'
@@ -15,6 +22,15 @@ function App() {
   const [gameTurns, setGameTurns] = useState([])
 
   const activePlayer = getActivePlayer(gameTurns)
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn
+    const { row, col } = square
+
+    gameBoard[row][col] = player
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns(prevTurns => {
@@ -35,7 +51,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
       </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
     </div>
       <Log turns={gameTurns} />
    </main>
